086b51f016ab858b9bf33f8271e2b24856620480
Mohamed T. Kassar <ktarek1994@gmail.com>
2017-07-15 18:55:44 +0200

    Update StopMachineException.java

--- src/upmc/machineuniverselle/exceptions/StopMachineException.java
+++ src/upmc/machineuniverselle/exceptions/StopMachineException.java
@@ -1,6 +1,9 @@
 package upmc.machineuniverselle.exceptions;
 
-
+/**
+ * @author KASSAR Mohamed Tarek
+ *
+ */
 
 public class StopMachineException extends UMException {
 	private static final long serialVersionUID = 8667284827498639867L;
