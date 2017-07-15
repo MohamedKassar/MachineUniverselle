c66cf07c4ab878091fad9f32ab049e39a1ca1b55
Mohamed T. Kassar <ktarek1994@gmail.com>
2017-07-15 18:55:24 +0200

    Update OperationNotSupportedException.java

--- src/upmc/machineuniverselle/exceptions/OperationNotSupportedException.java
+++ src/upmc/machineuniverselle/exceptions/OperationNotSupportedException.java
@@ -1,5 +1,9 @@
 package upmc.machineuniverselle.exceptions;
 
+/**
+ * @author KASSAR Mohamed Tarek
+ *
+ */
 
 public class OperationNotSupportedException extends UMException {
 	private static final long serialVersionUID = -3820630440550644029L;
