814482363b693c5944ef46fdb7ea8336b6b7052a
Mohamed T. Kassar <ktarek1994@gmail.com>
2017-07-15 18:56:02 +0200

    Update UMException.java

--- src/upmc/machineuniverselle/exceptions/UMException.java
+++ src/upmc/machineuniverselle/exceptions/UMException.java
@@ -1,5 +1,10 @@
 package upmc.machineuniverselle.exceptions;
 
+/**
+ * @author KASSAR Mohamed Tarek
+ *
+ */
+
 public abstract class UMException extends RuntimeException {
 
 	private static final long serialVersionUID = 7057337391915370415L;
